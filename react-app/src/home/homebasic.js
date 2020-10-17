dbaaed29d014a28bb880ba73ccbb6fc42dbbba18
sabethunder8 <sabalmulepati@gmail.com>
2020-10-17 15:36:03 -0700

    Revert "homebasic"
    This reverts commit eae6cc4f719d999eb37b9f8ab12b954c6fc6cdb3.

--- react-app/src/home/homebasic.js
+++ react-app/src/home/homebasic.js
@@ -4,17 +4,17 @@ import './homepage.css';
 
 function homebasic() {
     return (
-        <div className="homebasic">
+        <div>
             <head>
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
 
             </head>
             <body>
-                {/* Navigation */}
+                <!-- Navigation -->
                 
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 
-                {/* he drop down for a certain size */}
+                <!-- The drop down for a certain size -->
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -32,13 +32,13 @@ function homebasic() {
                 </div>
                 </nav>
 
-                {/* <!-- Search Bar --> */}
+                <!-- Search Bar -->
 
-                {/* <!-- Community cards -->  */}
+                <!-- Community cards --> 
                 
                 {/* <Card /> */}
 
-                {/* <!-- Optional JavaScript --> */}
+                <!-- Optional JavaScript -->
                 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
                 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
@@ -47,7 +47,4 @@ function homebasic() {
         </div>
     )
 }
-
-export default homebasic;
-
 
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import homebasic from './views/home/homebasic';
+import homebasic from './home/homebasic';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <homebasic />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
