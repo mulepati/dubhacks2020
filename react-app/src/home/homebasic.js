22af1194e85c5ff525a2dff81c4b95ca930cac71
sabethunder8 <sabalmulepati@gmail.com>
2020-10-17 14:32:07 -0700

    homepage

--- react-app/src/home/homebasic.js
+++ react-app/src/home/homebasic.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import './homepage.css';
 
-function homebasic () {
+
+function homebasic() {
     return (
         <div>
             <head>
@@ -8,6 +10,34 @@ function homebasic () {
 
             </head>
             <body>
+                <!-- Navigation -->
+                
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                
+                <!-- The drop down for a certain size -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href=".">Home </a>
+                        </li>
+                        <li class="nav-item" id="loginBar">
+                            <a class="nav-link" href=".">Login </a>
+                        </li>
+                    </ul>
+                    <a class="navbar-brand" href="#"><img src="react-app\src\img\blank-profile-picture-973460_1280.webp"/></a>
+                </div>
+                </nav>
+
+                <!-- Search Bar -->
+
+                <!-- Community cards --> 
+                
+                {/* <Card /> */}
+
                 <!-- Optional JavaScript -->
                 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
