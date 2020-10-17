a4cd521183207efa690eeee7256ee986cc57000f
Markus Schiffer <47703802+MarkusSchiffer@users.noreply.github.com>
2020-10-17 13:41:19 -0700

    added sample user

---
+++ react-app/src/groups/group.js
@@ -0,0 +1,12 @@
+class Group extends React.Component {
+    render() {
+      const category = this.props.category;
+      return (
+        <tr>
+          <th colSpan="2">
+            {category}
+          </th>
+        </tr>
+      );
+    }
+  }
---
+++ react-app/src/users/user.js
@@ -0,0 +1,7 @@
+class User extends React.Component {
+    render() {
+        return (
+            <h1>Sample User Name</h1>
+        )
+    }
+}
