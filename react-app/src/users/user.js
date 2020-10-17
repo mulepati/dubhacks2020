f21a73af6a0cfbc7ce8729b9e08a71e7e8610a85
Markus Schiffer <47703802+MarkusSchiffer@users.noreply.github.com>
2020-10-17 14:23:18 -0700

    model definitions

--- react-app/src/users/user.js
+++ react-app/src/users/user.js
@@ -1,4 +1,16 @@
+// UserName
+// Hobbies
+// Groups
+// 
+
 class User extends React.Component {
+    constructor() {
+        super();
+        this.state = {userName: };
+
+
+      }
+
     render() {
         return (
             <h1>Sample User Name</h1>
