b1d59e860d771b672b0978a37c69d80a05f94f12
sabethunder8 <sabalmulepati@gmail.com>
2020-10-17 15:39:15 -0700

    reset

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import homebasic from './home/homebasic';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <homebasic />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
