a6d2225a89ff7c838242dfa1d15143ad3d05fbf5
Markus Schiffer <47703802+MarkusSchiffer@users.noreply.github.com>
2020-10-17 14:43:48 -0700

    models

---
+++ react-app/src/models/user.js
@@ -0,0 +1,33 @@
+// UserName
+// UID
+// Communities / expirience
+// Groups
+// 
+
+class User extends React.Component {
+    constructor(id) {
+        super();
+        try {
+            let response = await fetch("http://localhost:4567/api/users?id=" + id);
+            if (!response.ok) {
+                alert(`Response to get user ${id} failed.`);
+                return;
+            }
+            let parsed = await response.json();
+            this.state ={
+                uID: parsed[0],
+                userName: parsed[1],
+                communities: generateCommunities(parsed[2])
+            };
+        } catch (e) {
+            alert(`Exception on user fetch request for user ${id}`);
+            console.log(e);
+        }
+      }
+
+    render() {
+        return (
+            <h1>this.state.uID</h1>
+        )
+    }
+}
